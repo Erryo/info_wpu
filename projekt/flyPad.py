61fbe9d7b880fe9564afdb4394b53add29881291
Erryo <159823096+Erryo@users.noreply.github.com>
2026-01-07 18:06:43 +0100

    Alter Code+Jump ersetzen

--- projekt/flyPad.py
+++ projekt/flyPad.py
@@ -1,13 +1,14 @@
 from djitellopy import tello
 import time
-import os
 
 drone = tello.Tello()
 
 
 def center_over_pad(pad):
-#    drone.go_xyz_speed_mid(130,0,20,50,0,pad,pad+1)
-    drone.go_xyz_speed_mid(0,0,30,20,pad)
+    drone.go_xyz_speed_mid(0,0,20,20,pad)
+    yaw = drone.get_yaw()
+    drone.rotate_counter_clockwise(yaw)
+    #drone.rotate_clockwise(yaw)
 
 
 def get_pad_position():
@@ -23,14 +24,14 @@ def polygon_area_from_vectors(vectors):
     # Reconstruct vertices
     x, y = 0, 0
     vertices = [(x, y)] # die Koordinaten der Ecken des Vieleck
-    
+
     # Berechne die Ecken
     for dx, dy in vectors:
         x += dx
         y += dy
         vertices.append((x, y))
     print("Vertices:",vertices)
-    
+
     # Shoelace formula aka. Gaußsche Trapezformel
     # NOTE: Ich verstehe diese Formel nicht, siehe dazu https://de.wikipedia.org/wiki/Gau%C3%9Fsche_Trapezformel
     area = 0
@@ -57,21 +58,12 @@ print("Batterie:", drone.get_battery(), "%")
 drone.takeoff()
 time.sleep(1)
 
-# Mission Pads aktivieren                                 
-drone.enable_mission_pads()                               
-drone.set_mission_pad_detection_direction(2)  # 2 = beide 
-
-drone.get_mission_pad_id()
-drone.get_mission_pad_id()
+# Mission Pads aktivieren
+drone.enable_mission_pads()
+drone.set_mission_pad_detection_direction(0)  # 2 = beide
 
 print("Warten auf Mission Pad 1 ...")
 pad = drone.get_mission_pad_id()
-start_pad = pad
-if start_pad <=0 :
-    print(f"EXIT due to mid = {start_pad}")
-    drone.land()
-    os._exit(1)
-#visited_pads = [pad]
 print(f"Pad {pad} erkannt!")
 
 
@@ -85,11 +77,9 @@ print("Starte Vorwärts-Navigation...\n")
 
 side_vectors=[] #  Vektoren, die zu den jeweiligen Seite von der letzte Seite führen
 # Endlosschleife bis Pad 1 erneut kommt
-dist_flown = 50
 while True:
     # Schrittweise vorwärts fliegen
-    drone.go_xyz_speed_mid(dist_flown,0,50,30,last_pad)
-    dist_flown += 50
+    drone.move_forward(50)
     time.sleep(0.5)
 
     # Aktuelles Pad abfragen
@@ -97,47 +87,49 @@ while True:
 
     # Aktueller Abstand zum Pad
     dist_current_pad = get_pad_position()
-    print(f"Entfernung zum Pad {pad} ist: {dist_current_pad}" )
+    print(dist_current_pad)
 
     # Hier entsprechen Pads den Ecken des Vieleck
-    
+
     # Nachdem ein neuer Pad gefunden wurde,
     # speichere die Steigung der neuer Seite
+    if dist_pad == (0,0):
+        print("Reseting the step_2d")
+        step_2d = dist_current_pad
 
     # Wenn kein Pad gefunden wurde,
     # aktualisiere den Abstand zum Pad
+    if pad == -1:
+        dist_pad=dist_current_pad
 
-    if pad != -1:   # Ein Pad wurde erkannt
+    elif pad != -1:   # Ein Pad wurde erkannt
         print(f"Mission Pad erkannt: {pad}")
 
         # Man konnte warscheinlich die If-Bedingung weglassen,
         # aber ich bin mir nicht sicher
         # Es konnte ,,Edge-Cases" geben
         if pad != last_pad: # Eine neue Ecke wurde gefunden, damit auch eine neue Seite
-            # Da der Abstand nun relativ zum neuen Pad ist, 
+            # Da der Abstand nun relativ zum neuen Pad ist,
             # hat man nicht den aktuelsten Abstand zum letzten Pad
             # und muss berechnet werden.
             # + step_2d macht genau das.
             # - dist_current_pad[0] falls die Drohne nicht genau auf dem neuen Pad steht
-            dist_flown -= drone.get_mission_pad_distance_x()
-            print(f"debug:{drone.get_mission_pad_distance_x()}")
-            print("Side done with len:",dist_flown)
-            
+            dist_pad = dist_pad[0] - dist_current_pad[0]+step_2d[0], dist_pad[1] - dist_current_pad[1]+step_2d[1]
+            print("Side done with len:",dist_pad)
             # speichere den neun Vektor
             side_vectors.append(dist_pad)
             dist_pad = (0,0)
-            dist_flown = 0
-            center_over_pad(pad)
-            
+
 
         # Wenn wieder Pad 1 → landen
-        if pad == start_pad and last_pad != start_pad:
-            print(f"Pad {start_pad} erneut erreicht → Landen...")
+        if pad == 1 and last_pad != 1:
+            print("Pad 1 erneut erreicht → Landen...")
             center_over_pad(pad)
             drone.land()
             break
 
         # Über dem Pad ausrichten
+        center_over_pad(pad)
 
         last_pad = pad
 
